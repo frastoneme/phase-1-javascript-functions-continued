7208554f27f67ce391d738deac91a7cc3aa54da9
frastoneme <oigoeme@gmail.com>
2024-03-23 12:49:04 +0300

    ovveride functions test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(nothing="roller-skate") {     //default activity when nothing is passed
+    return `This Saturday, I want to ${nothing}!`
+  }
+  let mondayWork = function(nothing="go to the office") {
+    return `This Monday, I will ${nothing}.`
+  }
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
